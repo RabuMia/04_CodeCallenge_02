dd8d2725e77f3dc2bf7b6d7104105e37b35bd8a8
RabuMia <Excelchen@hotmail.de>
2021-07-29 23:32:12 +0200

    Endlösung++

--- challenge_02.js
+++ challenge_02.js
@@ -16,28 +16,59 @@ Alter 13-17 // Cola
 
 /* Getränke-Challenge */
 
-let getraenke, alter;
+// getraenke = "Milch"; // <= kleiner als 5
+// getraenke = "Saft"; // <= kleiner als 12
+// getraenke = "Cola"; // <= kleiner als 17
+// getraenke = "Wein"; // >= kleiner als 18
 
-getraenke = "Milch"; // <= kleiner als 5
-getraenke = "Saft"; // <= kleiner als 12
-getraenke = "Cola"; // <= kleiner als 17
-getraenke = "Wein"; // >= kleiner als 18
+let getName, alter;
 
-switch (getraenke) 
-{
-    case ( alter <= 5):
-        console.log("Du darfst Milch trinken!")
-        break;
-    case ( alter <= 12):
-            console.log("Du darfst Saft trinken!")
-            break;
-    case ( alter <= 17):
-        console.log("Du darfst Cola trinken!")
-        break;
-    case ( alter >= 18):
-        console.log("Du darfst Wein trinken!")
-        break;
-    default:
-        console.log("Das war eine falsche Eingabe! Bitte gib ein Alter ein!")
-      break;
-}
+getName = prompt("Bitte gebe dein Name ein!");
+alter = prompt("Bitte gebe dein Alter ein!");
+
+
+// switch (true) 
+// {
+//     case ( alter <= 5):
+//         console.log("Du darfst Milch trinken!");
+//         break;
+//     case ( alter <= 12):
+//             console.log("Du darfst Saft trinken!");
+//             break;
+//     case ( alter <= 17):
+//         console.log("Du darfst Cola trinken!");
+//         break;
+//     case ( alter >= 18):
+//         console.log("Du darfst Wein trinken!");
+//         break;
+//     default:
+//         console.log("Das war eine falsche Eingabe! Bitte gib ein Alter ein!");
+//       break;
+// }
+
+if (alter <= 5) 
+
+ {  
+    console.log(getName + " darf Milch trinken!");   
+ } 
+
+ else if(alter <= 12)
+ {
+
+     console.log(getName + " darf Saft trinken!");   
+ } 
+
+ else if (alter <= 17)
+ {
+    
+     console.log(getName + " darf Cola trinken!"); 
+ }
+ else if (alter >= 18)
+ {
+    
+     console.log(getName + " darf Wein trinken!"); 
+ }
+ else 
+ {
+    console.log("Das war eine falsche Eingabe! Bitte gib ein Alter ein!"); 
+ }
