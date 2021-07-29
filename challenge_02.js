0494ad42b0fb1a61321333d2aea2b152df1d1d5c
RabuMia <Excelchen@hotmail.de>
2021-07-29 23:04:05 +0200

    Erster Versuch

--- challenge_02.js
+++ challenge_02.js
@@ -15,3 +15,29 @@ Alter 13-17 // Cola
 --> "Max trinkt Cola."
 
 /* Getränke-Challenge */
+
+let getraenke, alter;
+
+getraenke = "Milch"; // <= kleiner als 5
+getraenke = "Saft"; // <= kleiner als 12
+getraenke = "Cola"; // <= kleiner als 17
+getraenke = "Wein"; // >= kleiner als 18
+
+switch (getraenke) 
+{
+    case ( alter <= 5):
+        console.log("Du darfst Milch trinken!")
+        break;
+    case ( alter <= 12):
+            console.log("Du darfst Saft trinken!")
+            break;
+    case ( alter <= 17):
+        console.log("Du darfst Cola trinken!")
+        break;
+    case ( alter >= 18):
+        console.log("Du darfst Wein trinken!")
+        break;
+    default:
+        console.log("Das war eine falsche Eingabe! Bitte gib ein Alter ein!")
+      break;
+}
